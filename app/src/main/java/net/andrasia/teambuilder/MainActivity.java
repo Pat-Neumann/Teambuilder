7cfb5ee60a14515ed5b9109058525775532852af
Pat <patrick.neumann@stud.uni-regensburg,de>
2019-09-02 14:42:42 +0200

    Added Project Files

---
+++ app/src/main/java/net/andrasia/teambuilder/MainActivity.java
@@ -0,0 +1,14 @@
+package net.andrasia.teambuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
