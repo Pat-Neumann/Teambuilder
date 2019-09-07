3d5a33ba71d1c092d7071fec9019285d6edaef14
Pat <patrick.neumann@stud.uni-regensburg,de>
2019-09-07 17:18:49 +0200

    Added Login and Register

--- app/src/main/java/net/andrasia/teambuilder/LoginPage.java
+++ app/src/main/java/net/andrasia/teambuilder/LoginPage.java
@@ -2,21 +2,37 @@ package net.andrasia.teambuilder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
 
     Button mockToSearch;
+    Button login;
     Button loginPageBackBtn;
+    EditText emailEdit;
+    EditText passwordEit;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
         setupViews();
         setupListener();
 
@@ -25,6 +41,9 @@ public class LoginPage extends AppCompatActivity {
     private void setupViews() {
         mockToSearch = findViewById(R.id.mockButtonSearch);
         loginPageBackBtn = findViewById(R.id.loginPageBackBtn);
+        login = findViewById(R.id.loginPageLoginBtn);
+        emailEdit = findViewById(R.id.loginPageEmailEdit);
+        passwordEit = findViewById(R.id.loginPagePasswordEdit);
 
     }
 
@@ -43,5 +62,42 @@ public class LoginPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                loginUserAccount();
+            }
+        });
+    }
+
+    private void loginUserAccount() {
+        String email, password;
+        email = emailEdit.getText().toString();
+        password = passwordEit.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(LoginPage.this, SearchConfigurationPage.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/net/andrasia/teambuilder/RegisterPage.java
+++ app/src/main/java/net/andrasia/teambuilder/RegisterPage.java
@@ -2,14 +2,29 @@ package net.andrasia.teambuilder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterPage extends AppCompatActivity {
 
     Button registerBackBtn;
+    Button register;
+    EditText eMailEdit;
+    EditText passwordEdit;
+    EditText passwordConfirmEdit;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "RegisterPage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +33,16 @@ public class RegisterPage extends AppCompatActivity {
 
         setupViews();
         setupListener();
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     private void setupViews() {
-       registerBackBtn = findViewById(R.id.registerPageBackBt);
-        /*    register = findViewById(R.id.landingPageRegister); */
+        registerBackBtn = findViewById(R.id.registerPageBackBt);
+        register = findViewById(R.id.registerPageRegisterButton);
+        eMailEdit = findViewById(R.id.registerPageEmailEdit);
+        passwordEdit = findViewById(R.id.registerPagePasswordEdit);
+        passwordConfirmEdit = findViewById(R.id.registerPageConfirmPWEdit);
 
     }
 
@@ -33,19 +52,50 @@ public class RegisterPage extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(RegisterPage.this, LandingPage.class);
                 startActivity(intent);
+            }
+        });
 
-            }
-        });
-     /*
-     add button to register
-         register.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(LandingPage.this, RegisterPage.class);
-                startActivity(registerIntent);
+            public void onClick(View view){
+                registerNewUser();
             }
         });
-  */
     }
 
+    private void registerNewUser() {
+        String email, password;
+        email = eMailEdit.getText().toString();
+        if(passwordEdit.getText().toString().equals(passwordConfirmEdit.getText().toString())){
+            password = passwordEdit.getText().toString();
+        } else {
+            password = "";
+        }
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
 }
