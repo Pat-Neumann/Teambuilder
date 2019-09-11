72981193e1bb7583ed8f5d54ee5f23d9a6ae0ad8
Pat <patrick.neumann@stud.uni-regensburg,de>
2019-09-11 19:13:56 +0200

    LEave button removes user from database

--- app/src/main/java/net/andrasia/teambuilder/SearchPage.java
+++ app/src/main/java/net/andrasia/teambuilder/SearchPage.java
@@ -9,10 +9,19 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SearchPage extends AppCompatActivity {
 
     Button leaveQueueBtn;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,11 @@ public class SearchPage extends AppCompatActivity {
         setContentView(R.layout.search_page);
         setupViews();
         setupListener();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("User");
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
         /* Library used for the Loading Animation GIF, sjudd, 2017, https://github.com/bumptech/glide, accessed 05.09.2019 */
         ImageView loadingAnimation = findViewById(R.id.searchAnim);
@@ -44,10 +58,15 @@ public class SearchPage extends AppCompatActivity {
         leaveQueueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                removeUserFromDatabase();
                 Intent intent = new Intent(SearchPage.this, SearchConfigurationPage.class);
                 finish();
                 startActivity(intent);
             }
         });
+    }
+
+    private void removeUserFromDatabase() {
+        reference.child(user.getUid()).removeValue();
     }
 }
